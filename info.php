06e5a3028ca74d3d967cc13e18e6434efa1d5d11
FelishaDwina <felishadwina2005@mail.ugm.ac.id>
2025-12-22 07:49:54 +0700

    update info

--- info.php
+++ info.php
@@ -11,7 +11,7 @@
                 <div class="card-body">
                     <p>Sulawesi Tenggara adalah sebuah provinsi di Indonesia yang terletak di bagian tenggara Pulau Sulawesi. Provinsi ini memiliki kekayaan alam yang melimpah, mulai dari sumber daya mineral, kehutanan, hingga kelautan. Dengan garis pantai yang panjang dan dikelilingi oleh lautan, Sulawesi Tenggara memiliki potensi maritim yang sangat besar.</p>
                     <p>Ibu kota provinsi ini adalah Kendari. Sulawesi Tenggara dikenal dengan keindahan alam bawah lautnya, pulau-pulau eksotis, serta keunikan budaya lokal yang beragam. Sektor pertanian, perikanan, pertambangan, dan pariwisata menjadi tulang punggung perekonomian daerah ini.</p>
-                    <p><strong>(Tambahkan informasi lebih lanjut tentang geografi, budaya, atau potensi Sultra di sini.)</strong></p>
+                    <p><strong></strong></p>
                 </div>
             </div>
         </div>
@@ -25,7 +25,7 @@
                     <p>Keberadaan banyak pelabuhan di Sulawesi Tenggara tidak terlepas dari karakteristik geografis dan potensi ekonominya. Sebagai provinsi kepulauan dengan banyak pulau-pulau kecil dan garis pantai yang panjang, transportasi laut menjadi tulang punggung konektivitas antar wilayah dan distribusi barang.</p>
                     <p>Selain itu, Sulawesi Tenggara merupakan daerah kaya akan sumber daya alam, terutama nikel dan aspal. Pelabuhan-pelabuhan di sini berperan penting sebagai pintu gerbang ekspor hasil tambang dan komoditas lainnya. Pelabuhan juga mendukung sektor perikanan yang menjadi mata pencarian utama sebagian besar masyarakat pesisir.</p>
                     <p>Pelabuhan penyeberangan (ferry) juga banyak ditemukan untuk menghubungkan pulau-pulau besar dan kecil, memfasilitasi pergerakan penduduk dan logistik. Dengan demikian, banyaknya pelabuhan di Sultra adalah cerminan dari kebutuhan konektivitas, dukungan ekonomi, dan potensi maritim yang dimiliki provinsi ini.</p>
-                    <p><strong>(Tambahkan data spesifik, jenis-jenis pelabuhan utama, atau proyek pengembangan pelabuhan di Sultra di sini.)</strong></p>
+                    <p><strong></strong></p>
                 </div>
             </div>
         </div>
@@ -38,7 +38,7 @@
                 <div class="card-body">
                     <p><strong>Nama:</strong> Felisha Dwina</p>
                     <p><strong>NIM:</strong> 24/543891/SV/25284</p>
-                    <p><strong>(Tambahkan informasi kontak atau atribusi lainnya di sini.)</strong></p>
+                    <p><strong></strong></p>
                 </div>
             </div>
         </div>
